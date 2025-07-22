e3248705477652cc383245ac46bdf00585a898e9
Tim Magee <tmagee2020@gmail.com>
2025-07-22 17:16:57 -0400

    fully swapped requests out for Playwright

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,8 @@
-import requests
+import time
+import random
+import logging
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
 
 def main():
@@ -17,25 +20,25 @@ def main():
     path = "/jobs/search"
     query = "?query=Software+Engineer&remote_locations=North+America--US+Remote&office_locations=North+America--Atlanta&office_locations=North+America--Chicago&office_locations=North+America--New+York&office_locations=North+America--New+York+Privy+HQ&office_locations=North+America--San+Francisco+Bridge+HQ&office_locations=North+America--Seattle&office_locations=North+America--South+San+Francisco&office_locations=North+America--Washington+DC"
     
-    jobUrls = getJobUrls(baseUrl, path, query)
-    print(len(jobUrls), jobUrls)
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=50)
+        page = browser.new_page()
+        page.goto(baseUrl + path + query)
+        randomDelay()
+        html = page.inner_html('body')
+        jobUrls = getJobUrls(html, baseUrl)
+        print(len(jobUrls), "\n", jobUrls)
+
+
     return
 
 
-def getJobUrls(baseUrl: str, path: str, query: str) -> list[str]:
-    searchUrl = baseUrl + path + query
-    response = requests.get(searchUrl)
-    print(str(response.status_code))
-    if response.status_code != 200:
-        return
-    
-    html = response.content
+def getJobUrls(html: str, baseUrl: str) -> list[str]:
     soup = BeautifulSoup(html, "html.parser")
-
     jobs = soup.find_all("a", class_="Link JobsListings__link")
     jobUrls = []
 
-    for i, job in enumerate(jobs):
+    for job in jobs:
         jobUrl = baseUrl + job.get("href")
         jobUrls.append(jobUrl)
     
@@ -48,11 +51,17 @@ before getting job details, i should learn about response vs session
 def getJobDetails():
     return
 
-
+def randomDelay():
+    randomTime = random.uniform(1.5, 7)
+    logger.info(randomTime)
+    time.sleep(randomTime)
 
 
 
     
 if __name__ == "__main__":
     print(".\n.\n.\n.\n.\n")
+    logger = logging.getLogger("Jobert Scraper")
+    logging.basicConfig(level=logging.INFO)
+    #logging.basicConfig(level=logging.ERROR)
     main()

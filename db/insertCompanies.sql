6422f1d2e12b1ee6fd9226f05020d219455044b6
tmagee02 <tmagee2020@gmail.com>
2025-07-25 18:42:56 -0400

    partial completion of multiple companies. added companies to db, created xpath json, and started looping through companies

--- db/insertCompanies.sql
+++ db/insertCompanies.sql
@@ -1,2 +1,20 @@
 -- insert into Company (company_name, base_url, search_path, search_query)
--- values ('Stripe', 'https://stripe.com', '/jobs/search', '?query=Software+Engineer&remote_locations=North+America--US+Remote&office_locations=North+America--Atlanta&office_locations=North+America--Chicago&office_locations=North+America--New+York&office_locations=North+America--New+York+Privy+HQ&office_locations=North+America--San+Francisco+Bridge+HQ&office_locations=North+America--Seattle&office_locations=North+America--South+San+Francisco&office_locations=North+America--Washington+DC')
+-- values ('Stripe', 'https://stripe.com', '/jobs/search', '?query=Software+Engineer&remote_locations=North+America--US+Remote&office_locations=North+America--Atlanta&office_locations=North+America--Chicago&office_locations=North+America--New+York&office_locations=North+America--New+York+Privy+HQ&office_locations=North+America--San+Francisco+Bridge+HQ&office_locations=North+America--Seattle&office_locations=North+America--South+San+Francisco&office_locations=North+America--Washington+DC');
+
+-- insert into Company (company_name, base_url, search_path, search_query)
+-- values ('Airbnb', 'https://careers.airbnb.com', '/positions/', '?_search_input=Software%20Engineer&_offices=united-states&_jobs_sort=updated_at');
+
+-- insert into Company (company_name, base_url, search_path, search_query)
+-- values ('Block', 'https://block.xyz', '/careers/jobs', '?teams[]=Software%20Engineering');
+
+-- insert into Company (company_name, base_url, search_path, search_query)
+-- values ('Uber', 'https://www.uber.com', '/us/en/careers/list/', '?query=Software%20Engineer&location=USA-California-San%20Francisco&location=USA-California-Sunnyvale&location=USA-California-Culver%20City&location=USA-California-Los%20Angeles&location=USA-New%20York-New%20York&location=USA-New%20York-Bronx&location=USA-Illinois-Chicago&location=USA-Texas-Dallas&location=USA-Washington-Seattle&location=USA-Arizona-Phoenix&location=USA-Florida-Miami&location=USA-District%20of%20Columbia-Washington&location=USA-Massachusetts-Boston');
+
+-- insert into Company (company_name, base_url, search_path, search_query)
+-- values ('OpenAI', 'https://openai.com', '/careers/search/', '?q=Software+Engineer&l=07ed9191-5bc6-421b-9883-f1ac2e276ad7%2Ce8062547-b090-4206-8f1e-7329e0014e98%2Cbbd9f7fe-aae5-476a-9108-f25aea8f6cd2%2C16c48b76-8036-4fe3-a18f-e9d357395713%2C6252b4ed-714d-469a-a970-7a13101bac9d');
+
+-- insert into Company (company_name, base_url, search_path, search_query)
+-- values ('Databricks', 'https://www.databricks.com', '/company/careers/open-positions', '?department=Engineering&location=United%20States');
+
+-- insert into Company (company_name, base_url, search_path, search_query)
+-- values ('Apple', 'https://jobs.apple.com', '/en-us/search', '?search=Software+Engineer&sort=newest&location=united-states-USA');
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,12 +4,14 @@ import time
 import random
 import logging
 import sqlite3
+import json
 import pandas as pd
+from collections import defaultdict
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 
-def main():
+def main():    
     selectCompany = '''
         select * 
         from Company
@@ -18,37 +20,64 @@ def main():
     with sqlite3.connect('./db/jobert.db') as conn:
         dataframe = pd.read_sql_query(selectCompany, conn)
         print(dataframe)
+    
+    with open('./scraper/xpathCompany.json', 'r') as file:
+        data = json.load(file)
 
-    idCompany = int(dataframe.at[0, 'id'])
-    companyName = dataframe.at[0, 'company_name']
-    baseUrl = dataframe.at[0, 'base_url']
-    searchPath = dataframe.at[0, 'search_path']
-    query = dataframe.at[0, 'search_query']
+
+    urlRenderTypes = {}
+    xpathCompany = defaultdict(dict)
+    for company in data:
+        urlRenderTypes[company['companyName']] = company['urlRenderType']
+        xpathCompany[company['companyName']] = company['xpaths']
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=50)
         page = browser.new_page()
-        page.goto(baseUrl + searchPath + query)
-        randomDelay()
-        html = page.inner_html('body')
-        jobUrls = findJobUrls(html)
 
-        jobDetails = {}
-        for jobUrl in jobUrls:
-            if jobUrl not in jobDetails:
-                logger.info(f'Gathering job details for {jobUrl}. Good.')
-                page.goto(jobUrl)
-                page.locator('h1.Copy__title').first.wait_for(state='visible')
-                html = page.inner_html('body')
-                findJobDetails(html, jobDetails, jobUrl)
-                randomDelay()
-            else:
-                logger.info(
-                    f'Job details already gathered for {jobUrl}. Skipping.')
+        for row in dataframe.itertuples():
+            idCompany = row.id
+            companyName = row.company_name
+            baseUrl = row.base_url
+            searchPath = row.search_path
+            searchQuery = row.search_query
+            urlRenderType = urlRenderTypes[companyName]
+            print(companyName, urlRenderType)
+            xJobUrl = xpathCompany[companyName]['jobUrl']
+            xNextPage = xpathCompany[companyName]['nextPage']
+            xJobTitle = xpathCompany[companyName]['jobTitle']
+            xJobDesc = xpathCompany[companyName]['jobDesc']
+            xLocation = xpathCompany[companyName]['location']
+            xRemote = xpathCompany[companyName]['remote']
+            xDatePosted = xpathCompany[companyName]['datePosted']
+            page.goto(baseUrl + searchPath + searchQuery)
+            randomDelay()
+        # page.goto(baseUrl + searchPath + query)
+        # randomDelay()
+        # html = page.inner_html('body')
+        # jobUrls = findJobUrls(html)
 
-        writeJobDetailsToFile(jobDetails)
-        insertJobToDatabase(jobDetails, idCompany)
+        # jobDetails = {}
+        # for jobUrl in jobUrls:
+        #     if jobUrl not in jobDetails:
+        #         logger.info(f'Gathering job details for {jobUrl}. Good.')
+        #         page.goto(jobUrl)
+        #         page.locator('h1.Copy__title').first.wait_for(state='visible')
+        #         html = page.inner_html('body')
+        #         exposeJobDetails()
+        #         findJobDetails(html, jobDetails, jobUrl)
+        #         randomDelay()
+        #     else:
+        #         logger.info(
+        #             f'Job details already gathered for {jobUrl}. Skipping.')
 
+        # writeJobDetailsToFile(jobDetails)
+        # insertJobToDatabase(jobDetails, idCompany)
+
+    return
+
+
+def testMultipleCompanies():
     return
 
 
@@ -71,8 +100,7 @@ def exposeJobDetails():
 def findJobDetails(html: str, jobDetails: dict, jobUrl: str) -> None:
     soup = BeautifulSoup(html, 'html.parser')
     jobTitle = soup.find('h1', class_='Copy__title').get_text(strip=True)
-    jobDesc = soup.find('div', class_='RowLayout').get_text(
-        separator=' \n\n ', strip=True)
+    jobDesc = soup.find('div', class_='RowLayout').get_text(separator=' \n ', strip=True)
 
     offices = soup.find('p', string='Office locations')
     remote = soup.find('p', string='Remote locations')
@@ -87,18 +115,19 @@ def findJobDetails(html: str, jobDetails: dict, jobUrl: str) -> None:
 
 def insertJobToDatabase(jobDetails: dict, idCompany: int) -> None:
     with sqlite3.connect('./db/jobert.db') as conn:
+        cursor = conn.cursor()
         for jobUrl, (title, jobDesc, offices, remote) in jobDetails.items():
             insertJob = '''
                 insert into Job (job_url, title, job_desc, company_id)
                 values (?, ?, ?, ?)
                 '''
             try:
-                cursor = conn.cursor()
                 cursor.execute(insertJob, (jobUrl, title, jobDesc, idCompany))
                 logger.info(f'Inserting row into db for Job: {title}. Good.')
                 idJob = cursor.lastrowid
             except sqlite3.IntegrityError:
                 logger.info(f'Row in db for Job: {title} already exists. Skipping.')
+    return
 
 
 
@@ -108,10 +137,11 @@ def randomDelay() -> None:
     randomTime = random.uniform(1.5, 7)
     logger.debug(f'Random Delay: {randomTime} sec')
     time.sleep(randomTime)
+    return
 
 
 def writeJobDetailsToFile(jobDetails: dict) -> None:
-    with open('jobDetails.txt', 'w') as f:
+    with open('./scraper/jobDetails.txt', 'w') as f:
         for jobUrl, (title, jobDesc, offices, remote) in jobDetails.items():
             f.write(jobUrl)
             f.write(f'\n{title}\n')

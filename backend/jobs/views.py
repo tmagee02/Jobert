561c7ccf7f5cef27c3031607f5d395a805077d3b
tmagee02 <tmagee2020@gmail.com>
2025-07-30 10:34:48 -0400

    added basic job view

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Job
+
+
+def jobs(request):
+    recentJobs = Job.objects.order_by('-date_scraped')[:100]
+    output = '<br>'.join([f'| {job.min_experience} || {job.company.company_name} || {job.title} || {job.job_url} |' for job in recentJobs])
+    return HttpResponse(output)

1246eff001d6cd268e4551dd78daf2b8753d5be2
tmagee02 <tmagee2020@gmail.com>
2026-05-21 16:03:06 -0400

    margin fix

--- frontend/src/components/MetadataBox.jsx
+++ frontend/src/components/MetadataBox.jsx
@@ -30,7 +30,7 @@ export default function MetadataBox({
                 className="metadata-child flex items-start"
             >
                 <div className="w-4 flex justify-center">
-                    <i className="fa-solid fa-briefcase text-[16px] mt-[4px]" />
+                    <i className="fa-solid fa-briefcase text-[16px] mt-[3px]" />
                 </div>
                 <div className="ml-4">
                     <p className="font-medium text-base">Experience</p>
@@ -44,7 +44,7 @@ export default function MetadataBox({
                 className="metadata-child flex items-start"
             >
                 <div className="w-4 flex justify-center">
-                    <i className="fa-solid fa-calendar-days text-[16px] mt-[4px]" />
+                    <i className="fa-solid fa-calendar-days text-[16px] mt-[3px]" />
                 </div>
                 <div className="ml-4">
                     <p className="font-medium text-base">Retrieved</p>

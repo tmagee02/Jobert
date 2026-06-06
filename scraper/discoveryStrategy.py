55345841571017dcb19ef594c0cc756a6d906704
tmagee02 <tmagee2020@gmail.com>
2026-06-06 17:04:33 -0400

    minor discovery changes

--- scraper/discoveryStrategy.py
+++ scraper/discoveryStrategy.py
@@ -1,5 +1,5 @@
 from scraper.company import Company
-from playwright.sync_api import Page, Locator
+from playwright.sync_api import Page
 from scraper.utils import randomDelay
 
 class DiscoveryError(Exception):
@@ -17,33 +17,37 @@ def runDiscoveryStrategy(company: Company, page: Page):
 
     print(f'{company.name}: {len(company.urlDiscoveryStrategy)} discovery strategy steps required')
     for i, step in enumerate(company.urlDiscoveryStrategy):
-        print(f'\t{i+1}. {step['type']} --> {step['selector']}')
+        print(f"\t{i+1}. {step['type']} --> {step['selector']}")
         randomDelay()
         actions[step['type']](step, page)
 
 
 def textInput(step: dict, page: Page):
     inputElement = page.locator(step['selector'])
-    if not inputElement.count(): 
-        raise DiscoveryError(f'No inputElement found for selector({step['selector']})')
-    if inputElement.count() > 1:
-        raise DiscoveryError(f'More than 1 element found for selector({step['selector']}) ({inputElement.count()} found)')
+    count = inputElement.count()
+    if count == 0:
+        raise DiscoveryError(f"No inputElement found for selector({step['selector']})")
+    if count > 1:
+        raise DiscoveryError(f"More than 1 element found for selector({step['selector']}) ({inputElement.count()} found)")
         
     inputElement.type(step['text'])
     
 
 def click(step: dict, page: Page):
     locator = page.locator(step['selector'])
-    if not locator.count(): 
-        raise DiscoveryError(f'No element found for selector({step['selector']})')
-    if locator.count() > 1:
-        raise DiscoveryError(f'More than 1 element found for selector({step['selector']}) ({locator.count()} found)')
+    count = locator.count()
+    if count == 0:
+        raise DiscoveryError(f"No element found for selector({step['selector']})")
+    if count > 1:
+        raise DiscoveryError(f"More than 1 element found for selector({step['selector']}) ({locator.count()} found)")
+        
+    locator.click()
 
 
 def clickAll(step: dict, page: Page):
     locator = page.locator(step['selector'])
     if not locator.count(): 
-        raise DiscoveryError(f'No elements found for selector({step['selector']})')
+        raise DiscoveryError(f"No elements found for selector({step['selector']})")
 
     for i in range(locator.count()):
         element = locator.nth(i)

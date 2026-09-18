839df21508e410f696d010658c3619f92828da60
Tim Magee <tmagee2020@gmail.com>
2026-09-18 11:28:45 -0400

    removed old synchronous code

--- scraper/discoveryStrategy.py
+++ scraper/discoveryStrategy.py
@@ -1,7 +1,8 @@
 import logging
 from scraper.company import Company
 from playwright.sync_api import Page
-from scraper.utils import randomDelay, asyncRandomDelay
+from scraper.utils import asyncRandomDelay
+
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +11,6 @@ class DiscoveryError(Exception):
     pass
 
 
-#-------async
 async def asyncRunDiscoveryStrategy(company: Company, page: Page):    
     if not company.urlDiscoveryStrategy:
         logger.debug('%s: No discovery strategy required', company.name)
@@ -61,57 +61,7 @@ async def asyncClickAll(step: dict, page: Page):
         element = locator.nth(i)
         await element.click()
         await asyncRandomDelay(shortDelay=True)
-#-------async end
 
-# def runDiscoveryStrategy(company: Company, page: Page):
-#     if not company.urlDiscoveryStrategy:
-#         return print(f'{company.name}: No discovery strategy required')
-
-#     print(f'{company.name}: {len(company.urlDiscoveryStrategy)} discovery strategy steps required')
-#     for i, step in enumerate(company.urlDiscoveryStrategy):
-#         print(f"\t{i+1}. {step['type']} --> {step['selector']}")
-#         randomDelay()
-#         DISCOVERY_ACTIONS[step['type']](step, page)
-
-
-# def textInput(step: dict, page: Page):
-#     inputElement = page.locator(step['selector'])
-#     count = inputElement.count()
-#     if count == 0:
-#         raise DiscoveryError(f"No inputElement found for selector({step['selector']})")
-#     if count > 1:
-#         raise DiscoveryError(f"More than 1 element found for selector({step['selector']}) ({inputElement.count()} found)")
-        
-#     inputElement.type(step['text'])
-    
-
-# def click(step: dict, page: Page):
-#     locator = page.locator(step['selector'])
-#     count = locator.count()
-#     if count == 0:
-#         raise DiscoveryError(f"No element found for selector({step['selector']})")
-#     if count > 1:
-#         raise DiscoveryError(f"More than 1 element found for selector({step['selector']}) ({locator.count()} found)")
-        
-#     locator.click()
-
-
-# def clickAll(step: dict, page: Page):
-#     locator = page.locator(step['selector'])
-#     if not locator.count(): 
-#         raise DiscoveryError(f"No elements found for selector({step['selector']})")
-
-#     for i in range(locator.count()):
-#         element = locator.nth(i)
-#         element.click()
-#         randomDelay(shortDelay=True)
-
-
-# DISCOVERY_ACTIONS = {
-#     'TEXT_INPUT': textInput,
-#     'CLICK': click,
-#     'CLICK_ALL': clickAll
-# }
 
 ASYNC_DISCOVERY_ACTIONS = {
     'TEXT_INPUT': asyncTextInput,
--- scraper/jobUrls.py
+++ scraper/jobUrls.py
@@ -1,75 +1,14 @@
 import asyncio
-import random
 import logging
 from typing import Set, Tuple
 from scraper.discoveryStrategy import asyncRunDiscoveryStrategy
-from scraper.utils import randomDelay, asyncRandomDelay, timed
+from scraper.utils import asyncRandomDelay, timed
 from playwright.sync_api import Page, Locator
-from playwright.async_api import async_playwright, Playwright, Browser, TimeoutError
+from playwright.async_api import Browser, TimeoutError
 from urllib.parse import urljoin, urlparse
-import time
 from scraper.company import Company
 
 logger = logging.getLogger(__name__)
-
-# def collectCompanyJobUrls(page: Page, company: Company) -> list[Tuple[str, str]]:
-#     jobUrls = []
-
-#     paginationLimit = 9
-#     paginationButton = page.locator(company.xpaths['pagination'])
-#     while paginationLimit > 0 and company.paginationType and isClickable(paginationButton):
-#         if company.paginationType == 'Next Page': 
-#             jobUrls.extend(getVisibleUrls(page, company))
-
-#         paginationButton.click()
-#         randomDelay(True)
-#         try:
-#             page.locator(company.xpaths['pagination']).wait_for(timeout=5000)
-
-#             paginationLimit -= 1
-#             paginationButton = page.locator(company.xpaths['pagination'])
-#         except: 
-#             break
-    
-#     jobUrls.extend(getVisibleUrls(page, company))
-
-#     return jobUrls
-
-
-# def collectAllCompanyJobUrls(page: Page, companies: dict, oldJobUrls: Set[str]) -> list[Tuple[str, str]]:
-#     timeStart = time.perf_counter()
-#     jobUrls = []
-
-#     for company in companies.values():
-#         page.goto(company.searchUrl())
-#         randomDelay()
-#         runDiscoveryStrategy(company, page) 
-#         companyJobUrls = collectCompanyJobUrls(page, company)
-#         companyJobUrls = filterOldUrls(company.name, companyJobUrls, oldJobUrls)
-#         jobUrls.extend(companyJobUrls)
-
-#     timeEnd = time.perf_counter()
-#     timecollectAllCompanyJobUrls = timeEnd - timeStart
-#     print(f'\ncollectAllCompanyJobUrls Time: {timecollectAllCompanyJobUrls}\n')
-#     return jobUrls
-
-
-# def isClickable(paginationButton: Locator) -> bool:
-#     isRemoved = paginationButton.count() == 0
-#     isDisabled = paginationButton.get_attribute('disabled') is not None
-#     return not (isRemoved or isDisabled)
-
-
-# def getVisibleUrls(page: Page, company: Company) -> list[Tuple[str, str]]:
-#     visibleUrls = []
-#     elements = page.locator(company.xpaths['jobUrl'])
-#     for i in range(elements.count()):
-#         element = elements.nth(i)
-#         jobPath = element.get_attribute(company.urlAttributeType)
-#         jobPath = normalizeJobPath(company.searchPath, jobPath)
-
-#         visibleUrls.append((company.name, urljoin(company.baseUrl + company.searchPath, jobPath)))
-#     return visibleUrls
 
 
 '''
@@ -109,12 +48,6 @@ def filterOldUrls(companyName: str, companyJobUrls: str, oldJobUrls: Set[str]):
     return newUrls
 
 
-
-#----------async 
-#----------async 
-#----------async 
-#----------async 
-#----------async 
 @timed('collectAllCompanyJobUrls', debugOnly=False)
 async def asyncCollectAllCompanyJobUrls(browser: Browser, companies: dict, oldJobUrls: Set[str]):
     jobUrls = []
